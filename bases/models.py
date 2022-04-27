a78b67ce177c2983e9d405b2420683a2194a7824
Seonmo <tjsah18@gmail.com>
2022-04-26 20:41:23 -0400

    [#22] Created Bases app
    App for abstract class
    Adding Common abstract class

---
+++ bases/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Time Stamp Base 
+class TimeStampBase(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
